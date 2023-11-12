510abe009d89d0fa2e819e4cd173c00fc2ab5640
ÁlvaroMonsalveSerrano <alvaro.monsalve.serrano@gmail.com>
2023-11-12 19:57:38 +0100

    Limpieza

--- src/config_template.h
+++ src/config_template.h
@@ -1,2 +1,10 @@
 const char* ssid     = "TU_SSID";
 const char* password = "TU_PASSWORD";
+
+const char* MQTT_BROKER_ADRESS = "IP";
+const int   MQTT_PORT = 1883;
+
+const char* MQTT_CLIENT_NAME = "MQTT_CLIENT_NAME";
+
+const char* TOPIC_ESP32_OXIGEN_BINOMIO1   = "esp32/binomio1/oxigeno";
+const char* TOPIC_BRIDGE_BINOMIO1_ACCION1 = "bridge/binomio1/accion1";
